0be064e579a80d0b55c2f414288f371f13e288c8
YSY <shaoyuanyu@foxmail.com>
2025-09-01 23:20:11 +0800

    fix:修复后端git仓库问题

---
+++ backend/src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package io.github.shaoyuanyu.ttts
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
