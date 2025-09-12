ce64fb270afd5ed300c849664dca2876fe600ca0
YSY <shaoyuanyu@foxmail.com>
2025-09-12 09:36:34 +0800

    feat(后端):完善请求监控

--- backend/src/main/kotlin/io/github/shaoyuanyu/ttts/plugins/Monitoring.kt
+++ backend/src/main/kotlin/io/github/shaoyuanyu/ttts/plugins/Monitoring.kt
@@ -7,6 +7,8 @@ import io.ktor.server.request.path
 import io.ktor.server.auth.UserIdPrincipal
 import io.ktor.server.auth.principal
 import io.ktor.server.request.httpMethod
+import io.ktor.server.request.uri
+import io.ktor.server.request.queryString
 import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
@@ -18,7 +20,9 @@ fun Application.configureMonitoring() {
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
             val userAgent = call.request.headers["User-Agent"]
-            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+            val uri = call.request.uri
+            val queryString = call.request.queryString().let { if (it.isNotEmpty()) "?$it" else "" }
+            "Status: $status, HTTP method: $httpMethod, URI: $uri$queryString, User agent: $userAgent"
         }
         
         mdc("userId") { call ->
